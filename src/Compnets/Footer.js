15e87761d27f776fd0df168cd0998202a1a07745
Nirav40833 <147812051+Nirav40833@users.noreply.github.com>
2026-09-23 15:54:57 +0530

    Update Footer.js

--- src/Compnets/Footer.js
+++ src/Compnets/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
                             </div>
                             <div>
                                 Email
-                                <p className='fw-bold text-primary'>nvcreation.site@gmail.com</p>
+                                <p className='fw-bold text-primary'>anilgupta2847@gmail.com</p>
                             </div>
                             {/* <div className='socil_icons'>
                                 <i><FaFacebookF /></i>
